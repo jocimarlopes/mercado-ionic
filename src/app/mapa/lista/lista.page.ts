b6934c4c9507bd2773cf103804021a761d00a3dc
jocimarlopes <lopes.jocimar@gmail.com>
2020-03-24 00:07:52 -0300

    Atualizações - superFlamel

--- src/app/mapa/lista/lista.page.ts
+++ src/app/mapa/lista/lista.page.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaPage implements OnInit {
 
+  public items: Array<any>;
+
   constructor() { }
 
   ngOnInit() {
+    this.items = [
+        { nome: 'Arroz', valor: '4,49' },
+        { nome: 'Feijão', valor: '5,99' },
+        { nome: 'Macarrão Instantâneo', valor: '3,29' },
+        { nome: 'Bolachinha Oreo ', valor: '2,89' },
+        { nome: 'Bolachinha Trakinas', valor: '3,29' },
+    ];
   }
-
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  public mercados: Array<any>;
+
   constructor() {}
 
+  ngOnInit() {
+    this.mercados = [
+        { mercado: 'Avenida', cidade: 'Osório', img: 'avenida.png', valor: '50,00' },
+        { mercado: 'Dalpiaz', cidade: 'Osório', img: 'dalpiaz.png', valor: '50,00' },
+        { mercado: 'Nacional', cidade: 'Osório', img: 'nacional.png', valor: '50,00' },
+        { mercado: 'Leve Mais', cidade: 'Osório', img: 'levemais.png', valor: '50,00' },
+        { mercado: 'Dia', cidade: 'Osório', img: 'dia.jpg', valor: '50,00' },
+    ];
+  }
 }
